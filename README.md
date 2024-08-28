07205d29192a45aeedfe48fc1355ff0e207d7b1e
mikhaelcahyo013 <mikhaelcahyo013@gmail.com>
2024-08-28 18:18:53 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mikhaelcahyo013
---
+++ story_content/user.js
@@ -0,0 +1,27 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5wYjMDVLIj5":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+
+audio.src="musikku.mp3";
+
+
+audio.load();
+
+
+
+audio.play();
+
+
+audio.volume=1.0;
+}
+
